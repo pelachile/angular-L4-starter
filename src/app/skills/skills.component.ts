09d1397ea8d06dfc2d46e30ef71c742790cd3abe
Cory Moore <cory@corymoore.com>
2018-12-03 18:17:09 -0600

    initial commit

---
+++ src/app/skills/skills.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Skill } from '../models/skill';
+import { Level } from '../types/level.enum';
+
+@Component({
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.css']
+})
+export class SkillsComponent implements OnInit {
+
+  skills: Skill[] = [
+    {
+      id: 0,
+      name: 'Abstract Mathematics',
+      level: Level.Expert
+    },
+    {
+      id: 1,
+      name: 'Philosophy',
+      level: Level.Advanced
+    },
+    {
+      id: 2,
+      name: 'Developing Software',
+      level: Level.Expert
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
